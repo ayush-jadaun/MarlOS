061e3683f3f0cd12053bc817cd8603e86b7d3ba9
arnavraj-7 <arnavrajcodes@gmail.com>
2025-11-08 03:16:14 +0530

    add test for p2p security

--- agent/p2p/__init__.py
+++ agent/p2p/__init__.py
@@ -1,14 +1,14 @@
-"""
-P2P networking module
-"""
-from .node import P2PNode
-from .protocol import MessageType, BaseMessage, create_message
-from .discovery import PeerDiscovery
+# """
+# P2P networking module
+# """
+# from .node import P2PNode
+# from .protocol import MessageType, BaseMessage, create_message
+# from .discovery import PeerDiscovery
 
-__all__ = [
-    'P2PNode',
-    'MessageType',
-    'BaseMessage',
-    'create_message',
-    'PeerDiscovery'
-]
+# __all__ = [
+#     'P2PNode',
+#     'MessageType',
+#     'BaseMessage',
+#     'create_message',
+#     'PeerDiscovery'
+# ]
