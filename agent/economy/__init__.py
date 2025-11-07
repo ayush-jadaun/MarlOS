f746e64d12b48cdb6efedc41083f24c98c06014c
Ayush <ayushjadaun6@gmail.com>
2025-11-07 22:05:36 +0530

    create rl training policy ith fairness consideration, and 25 dimensions

---
+++ agent/economy/__init__.py
@@ -0,0 +1,24 @@
+"""
+Economy Package - Token Economics and Fairness
+"""
+from .fairness import (
+    EconomicFairnessEngine,
+    ProgressiveTaxation,
+    UniversalBasicIncome,
+    DiversityQuotas,
+    TrustDecay,
+    JobComplexityAnalyzer,
+    ProofOfWorkVerification,
+    CooperativeRewards
+)
+
+__all__ = [
+    'EconomicFairnessEngine',
+    'ProgressiveTaxation',
+    'UniversalBasicIncome',
+    'DiversityQuotas',
+    'TrustDecay',
+    'JobComplexityAnalyzer',
+    'ProofOfWorkVerification',
+    'CooperativeRewards'
+]
